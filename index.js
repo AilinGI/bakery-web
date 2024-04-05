82c39a365d7e8041d0adef062e2feebb27677787
Ailin Garrido Inostroza <ailingarrido@MacBook-Pro-de-Ailin.local>
2024-04-05 17:24:46 +0100

    add menu-products and description

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
-let slideIndex = 0;
+var contenedor = document.querySelector(".slider"),
+  btnIzquierdo = document.getElementById("btn-izquierda"),
+  btnDerecho = document.getElementById("btn-derecha");
 
-function showSlide(index) {
-  const slides = document.querySelectorAll(".slider img");
+btnDerecho.addEventListener("click", function () {
+  contenedor.scrollLeft += contenedor.offsetWidth;
+});
 
-  if (index >= slides.length) {
-    slideIndex = 0;
-  } else if (index < 0) {
-    slideIndex = slides.length - 1;
-  } else {
-    slideIndex = index;
-  }
-
-  slides.forEach((slide, i) => {
-    if (i === slideIndex) {
-      slide.style.transform = "translateX(0)";
-    } else {
-      slide.style.transform = "translateX(-100%)";
-    }
-  });
-}
-
-function prevSlide() {
-  showSlide(slideIndex - 1);
-}
-
-function nextSlide() {
-  showSlide(slideIndex + 1);
-}
-
-showSlide(slideIndex);
+btnIzquierdo.addEventListener("click", function () {
+  contenedor.scrollLeft -= contenedor.offsetWidth;
+});
